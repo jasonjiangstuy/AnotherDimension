7f607ec7a1e01f523cd5f81beea31b8589dc742d
jasonjiangstuy <jasonjiangny@gmail.com>
2020-10-25 22:45:41 -0400

    is row magic

--- ArrayOps.java
+++ ArrayOps.java
@@ -65,7 +65,23 @@ public class ArrayOps {
 
     }
 
-    public static boolean isRowMagic(int[][] matrix){}
+    // 5b
+    public static boolean isRowMagic(int[][] matrix){
+
+        int [] arr = sumRows(matrix);
+        if (arr.length <= 0) {
+            return true;
+        }
+        int hold = arr[0];
+
+        for (int i = 0; i < arr.length ; i++){
+            if (hold != arr[i]){
+                return false;
+            }
+        }
+        return true;
+
+    }
     public static boolean isColMagic(int[][] matrix) {}
     public static boolean isLocationMagic(int[][] matrix, int row, int col){}
 }
