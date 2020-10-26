b8e8f95fb3c63050a4d2ef31a490b3dd6a802f71
jasonjiangstuy <jasonjiangny@gmail.com>
2020-10-25 20:31:03 -0400

    sum finished

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,3 +1,15 @@
 public class ArrayOps {
-    
+
+
+    // 4a
+    public static int sum(int[] arr){
+        int myFinal = 0;
+        for (int i =0 ; i < arr.length; i++){
+            myFinal += arr[i];
+        }
+        return myFinal;
+    }
+
+
+
 }
--- Tester.java
+++ Tester.java
@@ -1,5 +1,20 @@
 public class Tester{
     public static void main(String[] args){
         
+        int [] test1 = {1, 2, 3, 4, 5};
+        int [] test2 = {5 , 4 , 3, 2, 1};
+        
+        
+    // testing sum
+        System.out.println(ArrayOps.sum(test1));
+        System.out.println(ArrayOps.sum(test2));
+        // 15
+
+
+
+
+
+
+
     }
 }
