a7c1a503cfc2b8a258878a386fe70641a933aa24
jasonjiangstuy <jasonjiangny@gmail.com>
2020-10-25 23:09:10 -0400

    primary tests success

--- Tester.java
+++ Tester.java
@@ -1,3 +1,6 @@
+import java.sql.Array;
+import java.util.Arrays;
+
 public class Tester{
     public static void main(String[] args){
         
@@ -15,8 +18,67 @@ public class Tester{
         System.out.println(ArrayOps.largest(test2));
         // 5
 
-    // 
+    int[][] A = { {  1,  0, 12, -1 },
+                  {  7, -2,  2,  1 },
+                  { -5, -2,  2, -9 }
+               };
+    int[] B = {  1, 3, 5 };
 
+    // sumRows
+        System.out.println( Arrays.toString(ArrayOps.sumRows(A)))   ;
+        // [12, 8, -14]
+    // largestInRows
+        System.out.println(Arrays.toString(ArrayOps.largestInRows(A)));
+        // [12, 7, 2]
+    // 2d sum
+    System.out.println( ArrayOps.sum(B)) ;
+    // 9
+    // sumCols
+    System.out.println( ArrayOps.sum(A));
+    // 6
+
+    int [][] fivetest1 = 
+    { {  1,  2, 3, 4 },
+    {  2, 3,  4,  1 },
+    { 3, 4,  1, 2 } };
+
+    int [][] fivetest2 = 
+    { {  1,  1, 1 },
+    {  2, 2, 2 },
+    { 3,  3, 3 } };
+
+    int [][] fivetest3 =
+    { {  2,  2, 2 },
+    {  2, 2, 2 } };
+
+    // isRowMagic
+    System.out.println( ArrayOps.isRowMagic(fivetest1));
+    // true
+    System.out.println( ArrayOps.isRowMagic(fivetest2));
+    // false
+    System.out.println( ArrayOps.isRowMagic(fivetest3));
+    // true
+
+    // isColMagic
+    System.out.println( ArrayOps.isColMagic(fivetest1));
+    // false
+    System.out.println( ArrayOps.isColMagic(fivetest2));
+    // true
+    System.out.println( ArrayOps.isColMagic(fivetest3));
+    // true
+
+
+
+    int[][]E = { 
+        {  2,  4, 2 }, 
+        {  2, 2, 2 } }; 
+
+    // isLocationMagic
+    System.out.println( ArrayOps.isLocationMagic(E, 0 , 1));
+    // false
+
+    System.out.println( ArrayOps.isLocationMagic(E, 1, 1));
+    // true
 
 
 
