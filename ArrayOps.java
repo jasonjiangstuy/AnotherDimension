aaf56308d0314b2f5f7762d72a5750e1a357295b
jasonjiangstuy <jasonjiangny@gmail.com>
2020-10-25 21:58:27 -0400

    finished largestInRow

--- ArrayOps.java
+++ ArrayOps.java
@@ -31,4 +31,24 @@ public class ArrayOps {
         return myFinal;
     }
 
+    // 4d
+    public static int[] largestInRows(int[][] matrix){
+        int[] myFinal = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++){
+            myFinal[i] = largest(matrix[i]);
+        }
+        return myFinal;
+    }
+
+    // 4e
+    public static int sum(int[][] arr) {
+        int[] myFinal = sumRows(arr);
+        return sum(myFinal);
+    }
+
+
+    public static int[] sumCols(int[][] matrix){}
+    public static boolean isRowMagic(int[][] matrix){}
+    public static boolean isColMagic(int[][] matrix) {}
+    public static boolean isLocationMagic(int[][] matrix, int row, int col){}
 }
